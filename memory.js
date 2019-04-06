8fada6dfcc6e2f52e31acf3a1324e24fcc669cb7
Innovoeb <Innovoeb@gmail.com>
2019-04-06 15:39:54 -0700

    styles1

--- memory.js
+++ memory.js
@@ -35,16 +35,28 @@ $(document).ready(function() {
   })
 
 
-  // onclick of cards
+
+  // game logic
   $('.play-field').on('click', '.card', function () {
       blaster.play();
-      $(this).html(`${cardArr[0].image}`)
+
+        // card flips but shows same image when clicked on a card
+  //    $(this).html(`${cardArr[0].image}`)
+
+
+
+
+
+
+
+
 
 
 
     if (cardA == null) {
       cardA = this.innerHTML
       pickA = this.id
+
 
 
       console.log("cardA:", cardA)
@@ -81,19 +93,19 @@ $(document).ready(function() {
       }
     }
 
+    // win/loss conditions
+      if (score == 9) {
+        cantina.play();
+        document.querySelector('.game-message').innerHTML = `The force is strong with this one!`
+        $('.card').attr('disabled', true)
+      }
 
-    if (score == 9) {
-    cantina.play();
-    document.querySelector('.game-message').innerHTML = `The force is strong with this one!`
-    $('.card').attr('disabled', true)
-    }
-
-    if (turns == 0) {
-    fates.play();
-    document.querySelector('.game-message').innerHTML = `Aren’t you a little short for a stormtrooper?`
-    $('.card').attr('disabled', true)
-    }
-  })
+      if (turns == 0) {
+        fates.play();
+        document.querySelector('.game-message').innerHTML = `Aren’t you a little short for a stormtrooper?`
+        $('.card').attr('disabled', true)
+      }
+    })
 
   // shuffle array function
   function shuffArr(array) {
@@ -110,24 +122,24 @@ $(document).ready(function() {
 
   // display cards and values into playfield div
   function placeOBJ() {
-    document.querySelector('#r1c1').innerHTML = `${cardArr[0].value}`
-    document.querySelector('#r1c2').innerHTML = `${cardArr[1].value}`
-    document.querySelector('#r1c3').innerHTML = `${cardArr[2].value}`
-    document.querySelector('#r1c4').innerHTML = `${cardArr[3].value}`
-    document.querySelector('#r1c5').innerHTML = `${cardArr[4].value}`
-    document.querySelector('#r1c6').innerHTML = `${cardArr[5].value}`
-    document.querySelector('#r2c1').innerHTML = `${cardArr[6].value}`
-    document.querySelector('#r2c2').innerHTML = `${cardArr[7].value}`
-    document.querySelector('#r2c3').innerHTML = `${cardArr[8].value}`
-    document.querySelector('#r2c4').innerHTML = `${cardArr[9].value}`
-    document.querySelector('#r2c5').innerHTML = `${cardArr[10].value}`
-    document.querySelector('#r2c6').innerHTML = `${cardArr[11].value}`
-    document.querySelector('#r3c1').innerHTML = `${cardArr[12].value}`
-    document.querySelector('#r3c2').innerHTML = `${cardArr[13].value}`
-    document.querySelector('#r3c3').innerHTML = `${cardArr[14].value}`
-    document.querySelector('#r3c4').innerHTML = `${cardArr[15].value}`
-    document.querySelector('#r3c5').innerHTML = `${cardArr[16].value}`
-    document.querySelector('#r3c6').innerHTML = `${cardArr[17].value}`
+    document.querySelector('#r1c1').innerHTML = `${cardArr[0].image}`
+    document.querySelector('#r1c2').innerHTML = `${cardArr[1].image}`
+    document.querySelector('#r1c3').innerHTML = `${cardArr[2].image}`
+    document.querySelector('#r1c4').innerHTML = `${cardArr[3].image}`
+    document.querySelector('#r1c5').innerHTML = `${cardArr[4].image}`
+    document.querySelector('#r1c6').innerHTML = `${cardArr[5].image}`
+    document.querySelector('#r2c1').innerHTML = `${cardArr[6].image}`
+    document.querySelector('#r2c2').innerHTML = `${cardArr[7].image}`
+    document.querySelector('#r2c3').innerHTML = `${cardArr[8].image}`
+    document.querySelector('#r2c4').innerHTML = `${cardArr[9].image}`
+    document.querySelector('#r2c5').innerHTML = `${cardArr[10].image}`
+    document.querySelector('#r2c6').innerHTML = `${cardArr[11].image}`
+    document.querySelector('#r3c1').innerHTML = `${cardArr[12].image}`
+    document.querySelector('#r3c2').innerHTML = `${cardArr[13].image}`
+    document.querySelector('#r3c3').innerHTML = `${cardArr[14].image}`
+    document.querySelector('#r3c4').innerHTML = `${cardArr[15].image}`
+    document.querySelector('#r3c5').innerHTML = `${cardArr[16].image}`
+    document.querySelector('#r3c6').innerHTML = `${cardArr[17].image}`
   }
 
 
