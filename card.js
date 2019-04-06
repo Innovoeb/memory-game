eb251eb04c787a2beed73b036e8b482c04d08293
Innovoeb <Innovoeb@gmail.com>
2019-04-06 13:36:02 -0700

    added card flip and win condition

--- card.js
+++ card.js
@@ -2,93 +2,91 @@
 var cardArr = [
   {
     name: "ackbar",
-    image: '<img src="./assets/akbar.jpg" width="150px">',
-
+    image: "<img src='./assets/akbar.jpg'>",
     value: 1
   },
   {
     name: "boba",
-    image: '<img src="./assets/bobafett.jpg" width="150px">',
+    image: "<img src='./assets/bobafett.jpg'>",
     value: 2
   },
   {
     name: "c3po",
-    image: '<img src="./assets/c3p.jpg" width="150px">',
+    image: "<img src='./assets/c3p.jpg'>",
     value: 3
   },
   {
     name: "chewie",
-    image: '<img src="./assets/chewy.jpg" width="150px">',
+    image: "<img src='./assets/chewy.jpg'>",
     value: 4
   },
   {
     name: "darth",
-    image: '<img src="./assets/anakin.jpg" width="150px">',
+    image: "<img src='./assets/anakin.jpg'>",
     value: 5
   },
   {
     name: "greedo",
-    image: '<img src="./assets/shot-too-late.jpg" width="150px">',
+    image: "<img src='./assets/shot-too-late.jpg'>",
     value: 6
   },
   {
     name: "han",
-    image: '<img src="./assets/shot-first.jpg" width="150px">',
+    image: "<img src='./assets/shot-first.jpg'>",
     value: 7
   },
   {
     name: "leia",
-    image: '<img src="./assets/princess.jpg" width="150px">',
+    image: "<img src='./assets/princess.jpg'>",
     value: 8
   },
   {
     name: "r2d2",
-    image: '<img src="./assets/droid.jpg" width="150px">',
+    image: "<img src='./assets/droid.jpg'>",
     value: 9
   },
   {
     name: "ackbar",
-    image: '<img src="./assets/akbar.jpg" width="150px">',
-
+    image: "<img src='./assets/akbar.jpg'>",
     value: 1
   },
   {
     name: "boba",
-    image: '<img src="./assets/bobafett.jpg" width="150px">',
+    image: "<img src='./assets/bobafett.jpg'>",
     value: 2
   },
   {
     name: "c3po",
-    image: '<img src="./assets/c3p.jpg" width="150px">',
+    image: "<img src='./assets/c3p.jpg'>",
     value: 3
   },
   {
     name: "chewie",
-    image: '<img src="./assets/chewy.jpg" width="150px">',
+    image: "<img src='./assets/chewy.jpg'>",
     value: 4
   },
   {
     name: "darth",
-    image: '<img src="./assets/anakin.jpg" width="150px">',
+    image: "<img src='./assets/anakin.jpg'>",
     value: 5
   },
   {
     name: "greedo",
-    image: '<img src="./assets/shot-too-late.jpg" width="150px">',
+    image: "<img src='./assets/shot-too-late.jpg'>",
     value: 6
   },
   {
     name: "han",
-    image: '<img src="./assets/shot-first.jpg" width="150px">',
+    image: "<img src='./assets/shot-first.jpg'>",
     value: 7
   },
   {
     name: "leia",
-    image: '<img src="./assets/princess.jpg" width="150px">',
+    image: "<img src='./assets/princess.jpg'>",
     value: 8
   },
   {
     name: "r2d2",
-    image: '<img src="./assets/droid.jpg" width="150px">',
+    image: "<img src='./assets/droid.jpg'>",
     value: 9
   }]
