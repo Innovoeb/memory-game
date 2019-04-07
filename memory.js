0f468e49c262c460f56b99e610d09cb1f59b188e
Innovoeb <Innovoeb@gmail.com>
2019-04-06 21:15:34 -0700

    prototype1: added blaster efx and cleaned up comments

--- memory.js
+++ memory.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
   var pickA
   var pickB
   var score = 0
-  var turns = 10
+  var turns = 20
   var blaster = new Audio('./assets/blaster.mp3');
   var cantina = new Audio('./assets/cantina.mp3');
   var fates = new Audio('./assets/fates.mp3');
@@ -86,21 +86,12 @@ $(document).ready(function() {
           })
       }
 
-//    $('.card').prop('disabled', false)
-    // $('.card').addClass('hide')
-
-//    shuffArr(cardArr)
-//    placeOBJ()
-    // var cardA = ""
-    // var cardB = ""
-    // var pickA = ""
-    // var pickB = ""
-    // var score = 0
-    // var turns = 10
 
 
-    // Add event listener to grid
+
+      // Add event listener to grid
         grid.addEventListener('click', function(event) {
+          blaster.play()
       // The event target is our clicked item
             let clicked = event.target
 
@@ -144,149 +135,23 @@ $(document).ready(function() {
                 previousTarget = clicked;
                 // scoreboard
                 document.querySelector('#score').innerHTML = `Score: ${score}`
-                document.querySelector('#turns').innerHTML = `${turns} Turns Left`
+                document.querySelector('#turns').innerHTML = `${turns} Guess Left`
             }
             //win/loss conditions
                 if (score == 9) {
                   cantina.play();
                   document.querySelector('.game-message').innerHTML = `The force is strong with this one!`
-                  $('.card').attr('disabled', true)
+                //  $('.card').attr('disabled', true)
                 }
 
 
                 if (turns == 0) {
                   fates.play();
                   document.querySelector('.game-message').innerHTML = `Aren’t you a little short for a stormtrooper?`
-                  $('.card').attr('disabled', true)
+              //    $('.card').attr('disabled', true)
                 }
 
           })
 
-
-
-
-
-
-    })
-
-
-
-  // game logic
-  // $('.play-field').on('click', '.card', function () {
-  //     blaster.play();
-  //
-  //       // card flips but shows same image when clicked on a card
-  // //    $(this).html(`${cardArr[0].image}`)
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //   // $(this).removeClass('hide')
-  //   blaster.play();
-  //   if (cardA == null) {
-  //     cardA = this.innerHTML
-  //     pickA = this.id
-  //     // $(this).prop('disabled', true)
-  //     // blaster.play();
-  //
-  //
-  //     console.log("cardA:", cardA)
-  //     console.log("pickA:", pickA)
-  //   } else if (cardB == null) {
-  //     cardB = this.innerHTML
-  //     pickB = this.id
-  //
-  //     // $(this).prop('disabled', true)
-  //     // blaster.play();
-  //
-  //     console.log("cardB:", cardB)
-  //     console.log("pickB:", pickB)
-  //     if (cardA == cardB) {
-  //       score += 1
-  //       cardA = null
-  //       cardB = null
-  //       shuffArr(praise)
-  //       document.querySelector('.game-message').innerHTML = `${praise[0]}`
-  //       document.querySelector('#score').innerHTML = `Score: ${score}`
-  //       document.querySelector('#turns').innerHTML = `${turns} Turns Left`
-  //
-  //       console.log("score:", score)
-  //
-  //       if (score === 9) {
-  //         cantina.play();
-  //         document.querySelector('.game-message').innerHTML = `The force is strong with this one!`
-  //         $('.card').prop('disabled', true)
-  //         $("#start").show()
-  //       }
-  //     } else {
-  //       turns--
-  //       // $(pickA).removeClass('show')
-  //       // $(pickB).removeClass('show')
-  //       // $(pickA).prop('disabled', false)
-  //       // $(pickB).prop('disabled', false)
-  //       cardA = null
-  //       cardB = null
-  //       shuffArr(encourage)
-  //       document.querySelector('.game-message').innerHTML = `${encourage[0]}`
-  //       document.querySelector('#score').innerHTML = `Score: ${score}`
-  //       document.querySelector('#turns').innerHTML = `${turns} Turns Left`
-  //
-  //       console.log("turns:", turns)
-  //
-  //       if (turns === 0) {
-  //         fates.play();
-  //         document.querySelector('.game-message').innerHTML = `Aren’t you a little short for a stormtrooper?`
-  //         $('.card').prop('disabled', true)
-  //         $("#start").show()
-  //       }
-  //     }
-  //   }
-  //
-  //
-
-
-  // shuffle array function
-  // function shuffArr(array) {
-  //   var ctr = array.length, temp, index;
-  //   while (ctr > 0) {
-  //     index = Math.floor(Math.random() * ctr)
-  //     ctr--
-  //     temp = array[ctr]
-  //     array[ctr] = array[index]
-  //     array[index] = temp
-  //   }
-  //   return array
-  // }
-
-  // display cards and values into playfield div
-  // function placeOBJ() {
-  //   document.querySelector('#r1c1').innerHTML = `${cardArr[0].image}`
-  //   document.querySelector('#r1c2').innerHTML = `${cardArr[1].image}`
-  //   document.querySelector('#r1c3').innerHTML = `${cardArr[2].image}`
-  //   document.querySelector('#r1c4').innerHTML = `${cardArr[3].image}`
-  //   document.querySelector('#r1c5').innerHTML = `${cardArr[4].image}`
-  //   document.querySelector('#r1c6').innerHTML = `${cardArr[5].image}`
-  //   document.querySelector('#r2c1').innerHTML = `${cardArr[6].image}`
-  //   document.querySelector('#r2c2').innerHTML = `${cardArr[7].image}`
-  //   document.querySelector('#r2c3').innerHTML = `${cardArr[8].image}`
-  //   document.querySelector('#r2c4').innerHTML = `${cardArr[9].image}`
-  //   document.querySelector('#r2c5').innerHTML = `${cardArr[10].image}`
-  //   document.querySelector('#r2c6').innerHTML = `${cardArr[11].image}`
-  //   document.querySelector('#r3c1').innerHTML = `${cardArr[12].image}`
-  //   document.querySelector('#r3c2').innerHTML = `${cardArr[13].image}`
-  //   document.querySelector('#r3c3').innerHTML = `${cardArr[14].image}`
-  //   document.querySelector('#r3c4').innerHTML = `${cardArr[15].image}`
-  //   document.querySelector('#r3c5').innerHTML = `${cardArr[16].image}`
-  //   document.querySelector('#r3c6').innerHTML = `${cardArr[17].image}`
-  // }
-
-
-
+        })
 })
