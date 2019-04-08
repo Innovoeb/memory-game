9560cc5311f8b61146532687e76126cb31418916
Nova <xplodnstar@live.com>
2019-04-07 21:10:30 -0700

    minor position changes, added the messeges back in.

--- card.js
+++ card.js
@@ -2,50 +2,50 @@
 var cardArr = [
   {
     name: "ackbar",
-    image:'./assets/akbar.jpg',
+    image: './assets/ackbar.jpg',
 
   },
   {
     name: "boba",
-    image:'./assets/bobafett.jpg',
+    image: './assets/boba.jpg',
 
   },
   {
     name: "c3po",
-    image:'./assets/c3p.jpg',
+    image: './assets/c3po.jpg',
 
   },
   {
     name: "chewie",
-    image:'./assets/chewy.jpg',
+    image: './assets/chewie.jpg',
 
   },
   {
     name: "darth",
-    image:'./assets/anakin.jpg',
+    image: './assets/darth.jpg',
 
   },
   {
     name: "greedo",
-    image:'./assets/shot-too-late.jpg',
+    image: './assets/greedo.jpg',
 
   },
   {
     name: "han",
-    image:'./assets/shot-first.jpg',
+    image: './assets/han.jpg',
 
   },
   {
     name: "leia",
-    image:'./assets/princess.jpg',
+    image: './assets/leia.jpg',
 
   },
   {
     name: "r2d2",
-    image:'./assets/droid.jpg',
+    image: './assets/r2d2.jpg',
 
   },
-  ]
+]
 
 // Duplicate array to create a match for each card
 let gameGrid = cardArr.concat(cardArr)
